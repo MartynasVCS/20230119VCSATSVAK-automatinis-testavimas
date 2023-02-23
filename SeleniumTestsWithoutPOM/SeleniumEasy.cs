fab7270d9fe29b362ae7fabd0a8deb3f798bf597
MartynasVCS <martynasvcs@gmail.com>
2023-02-23 21:17:57 +0200

    Refactor duplicated methods to use TestCase annotation

--- SeleniumTestsWithoutPOM/SeleniumEasy.cs
+++ SeleniumTestsWithoutPOM/SeleniumEasy.cs
@@ -28,38 +28,10 @@ namespace SeleniumTestsWithoutPOM
             Assert.AreEqual(expectedResult, actualResult);
         }
 
-        [Test]
-        public void SimpleFormDemoTwoInputFieldsInputs_5_And_2()
+        [TestCase("5", "2", "7")]
+        [TestCase("Hello", "2", "NaN")]
+        public void SimpleFormDemoTwoInputFields(string valueA, string valueB, string expectedResult)
         {
-            string valueA = "5";
-            string valueB = "2";
-            string expectedResult = "7";
-            
-            IWebDriver driver = new ChromeDriver();
-            driver.Url = "https://demo.seleniumeasy.com/basic-first-form-demo.html";
-
-            IWebElement inputA = driver.FindElement(By.XPath("//*[@id='sum1']"));
-            IWebElement inputB = driver.FindElement(By.XPath("//*[@id='sum2']"));
-            IWebElement buttonGetTotal = driver.FindElement(By.XPath("//*[@id='gettotal']/button"));
-            IWebElement spanTotal = driver.FindElement(By.XPath("//*[@id='displayvalue']"));
-
-            inputA.SendKeys(valueA);
-            inputB.SendKeys(valueB);
-            buttonGetTotal.Click();
-            string actualResult = spanTotal.Text;
-
-            driver.Quit();
-
-            Assert.AreEqual(expectedResult, actualResult);
-        }
-
-        [Test]
-        public void SimpleFormDemoTwoInputFieldsInputs_Hello_And_2()
-        {
-            string valueA = "Hello";
-            string valueB = "2";
-            string expectedResult = "NaN";
-
             IWebDriver driver = new ChromeDriver();
             driver.Url = "https://demo.seleniumeasy.com/basic-first-form-demo.html";
 
