8c6db032247cafb51699c74cae00207591405fcd
MartynasVCS <martynasvcs@gmail.com>
2023-02-23 20:05:55 +0200

    Add basic selenium test

---
+++ SeleniumTestsWithoutPOM/HelloWorld.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace SeleniumTestsWithoutPOM
+{
+    internal class HelloWorld
+    {
+        [Test]
+        public void FirstChromeTest()
+        {
+            string expectedResult = "Google";
+            
+            IWebDriver driver = new ChromeDriver();
+            driver.Url = "https://www.google.com/";
+            string actualResult = driver.Title;
+            driver.Quit();
+
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+    }
+}
