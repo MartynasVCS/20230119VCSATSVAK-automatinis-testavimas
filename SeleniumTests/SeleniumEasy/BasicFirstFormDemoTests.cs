11bcbe3677ac751f18eb508d4fbf76e4da0840a1
MartynasVCS <martynasvcs@gmail.com>
2023-02-27 20:58:21 +0200

    Implement setup and teardown methods

--- SeleniumTests/SeleniumEasy/BasicFirstFormDemoTests.cs
+++ SeleniumTests/SeleniumEasy/BasicFirstFormDemoTests.cs
@@ -6,37 +6,40 @@ namespace SeleniumTests.SeleniumEasy
 {
     internal class BasicFirstFormDemoTests
     {
+        [SetUp]
+        public void Setup()
+        {
+            Driver.SetupDriver();
+            Driver.OpenUrl("https://demo.seleniumeasy.com/basic-first-form-demo.html");
+        }
+
         [Test]
         public void SimpleFormDemoSingleInputField()
         {
             string expectedResult = "Hello World";
-
-            Driver.SetupDriver();
-            Driver.OpenUrl("https://demo.seleniumeasy.com/basic-first-form-demo.html");
 
             BasicFirstFormDemo.EnterMessage(expectedResult);
             BasicFirstFormDemo.ClickShowMessage();
             string actualResult = BasicFirstFormDemo.GetMessage();
 
             Assert.AreEqual(expectedResult, actualResult);
-
-            Driver.QuitDriver();
         }
 
         [TestCase("5", "2", "7")]
         [TestCase("Hello", "2", "NaN")]
         public void SimpleFormDemoTwoInputFields(string valueA, string valueB, string expectedResult)
         {
-            Driver.SetupDriver();
-            Driver.OpenUrl("https://demo.seleniumeasy.com/basic-first-form-demo.html");
-
             BasicFirstFormDemo.EnterValueA(valueA);
             BasicFirstFormDemo.EnterValueB(valueB);
             BasicFirstFormDemo.ClickGetTotal();
             string actualResult = BasicFirstFormDemo.GetTotal();
 
             Assert.AreEqual(expectedResult, actualResult);
+        }
 
+        [TearDown]
+        public void Teardown()
+        {
             Driver.QuitDriver();
         }
     }
