266faba8766aed4057427c54c0da1122093a664e
MartynasVCS <martynasvcs@gmail.com>
2023-03-08 22:11:33 +0200

    Add sample code for an api test

---
+++ ApiTests/ChuckNorrisApi.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+using System.Net;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace ApiTests
+{
+    public class ChuckNorrisApi
+    {
+        [Test]
+        public async Task GetRandomJoke()
+        {
+            HttpClient client = new HttpClient();
+            HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Get, "https://api.chucknorris.io/jokes/random");
+            HttpResponseMessage response = await client.SendAsync(request);
+            
+            string content = await response.Content.ReadAsStringAsync();
+
+            ChuckNorrisJoke joke = JsonSerializer.Deserialize<ChuckNorrisJoke>(content);
+            //using Newtonsoft.Json;
+            //ChuckNorrisJoke joke = JsonConvert.DeserializeObject<ChuckNorrisJoke>(content);
+
+            Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
+            StringAssert.Contains("assets.chucknorris.host", joke.icon_url);
+            StringAssert.Contains("api.chucknorris.io", joke.url);
+        }
+    }
+}
---
+++ ApiTests/ChuckNorrisJoke.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace ApiTests
+{
+    internal class ChuckNorrisJoke
+    {
+        public List<string> categories { get; set; }
+        public string created_at { get; set; }
+        public string icon_url { get; set; }
+        public string id { get; set; }
+        public string updated_at { get; set; }
+        public string url { get; set; }
+        public string value { get; set; }
+    }
+}
